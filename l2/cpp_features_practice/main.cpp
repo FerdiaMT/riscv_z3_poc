2c95f841d038583e50c78d914a6ceeecf9f7097c
FerdiaMT <ferdiatreacy@gmail.com>
2026-02-14 20:52:02 +0000

    trying const auto& for looping

--- l2/cpp_features_practice/main.cpp
+++ l2/cpp_features_practice/main.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main()
 {
-    std::cout << " C++ Feature practice" << std::endl;
+    std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+    for(int i = 0 ; i < numbers.size(); i++)
+    {
+        std::cout<<numbers.at(i)<<std::endl;
+    }
+
+    std::cout<<std::endl;
+
+    for(int num : numbers)
+    {
+        std::cout << num << std::endl;
+    }
+
+    std::cout<<std::endl;
+
+    for(auto num : numbers)
+    {
+        std::cout<< num << std::endl;
+    }
+
+    //auto looping through a vector of strings
+
+    std::vector<std::string> words = {"hello", "world", "andstuff"};
+
+    for (auto word : words) //causes a copy
+    {
+        std::cout<< word << std::endl;
+    }
+
+    std::cout<<std::endl;
+
+    for(const auto& word : words) // this is the best way to do it
+    {
+        std::cout<< word << std::endl;
+    }
+
+    // how come this isnt dereferenced
+
+    std::string str= "helloworld3";
+    std::string& str2  = str;
+
+    std::cout<< *str2 << std::endl;
+
     return 0;
 }
