81eb874f74a66cdde5dc3aaeb466c4aafcaea5f3
FerdiaMT <ferdiatreacy@gmail.com>
2026-02-14 23:20:21 +0000

    Basic excercise of replicating AST tree from test_ast in the output from main

---
+++ l3/build/CMakeFiles/astTree.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/astTree.dir/link.d"
+  "CMakeFiles/astTree.dir/main.cpp.o"
+  "CMakeFiles/astTree.dir/main.cpp.o.d"
+  "astTree"
+  "astTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/astTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ l3/build/CMakeFiles/astTree.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for astTree.
+# This may be replaced when dependencies are built.
