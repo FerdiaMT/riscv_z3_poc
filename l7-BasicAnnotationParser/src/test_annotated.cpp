de3305474ff771f528de2fba32871f34e6f200d5
FerdiaMT <ferdiatreacy@gmail.com>
2026-02-19 19:14:16 +0000

    added little note that is archive

--- l7-BasicAnnotationParser/src/test_annotated.cpp
+++ l7-BasicAnnotationParser/src/test_annotated.cpp
@@ -3,5 +3,19 @@
 int add(int x)
 {
     int result = x+1;
+    result -=1;
+    result +=1;
     return result;
 }
+
+[[clang::annotate("requires: x > 0")]]
+[[clang::annotate("ensures: result > x")]]
+int add_quick(int x)
+{
+    return x+1;
+}
+
+// Can i generate an smt in the most optimal way ??
+
+// arxiv.org/abs/2411.11469 arvix
+// ^ look at this f ei feeeeeeel like this
